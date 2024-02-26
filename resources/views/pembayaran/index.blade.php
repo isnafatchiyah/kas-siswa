9480b7ea3fcd76f80072a0f5aec4cd0a914dfd63
isnafatchiyah <isnafatchiyah05@gmail.com>
2024-02-26 11:12:42 +0700

    first commit

--- resources/views/pembayaran/index.blade.php
+++ resources/views/pembayaran/index.blade.php
@@ -12,31 +12,12 @@
 
 <body style="background: gray">
 
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container-fluid">
-
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="{{ route('siswa.index') }}">SISWA</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="{{ route('pembayaran.index') }}">PEMBAYARAN</a>
-                    </li>
-                </ul>
-            </div>
-    </nav>
-
-
     <div class="container mt-5">
         <div class="row">
             <div class="col-md-12">
                 <div class="card border-0 shadow rounded">
                     <div class="card-body">
-
+                        <a href="{{ route('siswa.index') }}" class="btn btn-md btn-success mb-3">SISWA</a>
                         <a href="{{ route('pembayaran.create') }}" class="btn btn-md btn-success mb-3">TAMBAH PEMBAYARAN</a>
                         <table class="table table-bordered">
                             <thead>
--- resources/views/siswa/index.blade.php
+++ resources/views/siswa/index.blade.php
@@ -9,29 +9,13 @@
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
 </head>
 <body style="background: grey">
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container-fluid">
-
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="{{ route('siswa.index') }}">SISWA</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="{{ route('pembayaran.index') }}">PEMBAYARAN</a>
-                    </li>
-                </ul>
-            </div>
-    </nav>
 
     <div class="container mt-5">
         <div class="row">
             <div class="col-md-12">
                 <div class="card border-0 shadow rounded">
                     <div class="card-body">
+                        <a href="{{ route('pembayaran.index') }}" class="btn btn-md btn-success mb-3">PEMBAYARAN</a>
                         <a href="{{ route('siswa.create') }}" class="btn btn-md btn-success mb-3">TAMBAH SISWA</a>
                         <table class="table table-bordered">
                             <thead>
